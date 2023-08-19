8bf1ba28db3fb1e717f2a7870c36ee4772e01ac2
Favour Nwachukwu <nwachukwufavour017@gmail.com>
2023-08-19 20:50:26 +0100

    added mvnTest script

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -4,6 +4,4 @@ def call(Map stageParams) {
             branches: [[name: stageParams.branch]],
             userRemoteConfigs: [[credentialsId: stageParams.credentialsId, url: stageParams.url]]
     ])
-
-//    checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: '63b9f1a8-ffe7-44fd-a97e-e4178f20d68e', url: 'https://github.com/jokermario/drone.git']])
 }
