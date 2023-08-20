2a126365f8f44256fc90d3e5e8ddc336c399c773
Favour Nwachukwu <nwachukwufavour017@gmail.com>
2023-08-20 16:20:49 +0100

    Added support to push image to aws ecr

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,14 +1,21 @@
-def call(String imageName, String imageTag, String dockerHubUser) {
-    withCredentials([usernamePassword(
-            credentialsId: 'docker',
-            passwordVariable: 'PASS',
-            usernameVariable: 'USER')]) {
+//def call(String imageName, String imageTag, String dockerHubUser) {
+//    withCredentials([usernamePassword(
+//            credentialsId: 'docker',
+//            passwordVariable: 'PASS',
+//            usernameVariable: 'USER')]) {
+//
+//        sh "docker login -u '$USER' -p '$PASS'"
+//    }
+//
+//    sh """
+//        docker image push ${dockerHubUser}/${imageName}:${imageTag}
+//        docker image push ${dockerHubUser}/${imageName}:latest
+//    """
+//}
 
-        sh "docker login -u '$USER' -p '$PASS'"
-    }
-
+def call(String ecrRepoName, String aws_account_id, String aws_region){
     sh """
-        docker image push ${dockerHubUser}/${imageName}:${imageTag}
-        docker image push ${dockerHubUser}/${imageName}:latest
+        aws ecr get-login-password --region ${aws_region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com
+        docker push ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecrRepoName}:latest
     """
 }
