cdf1e9ca9da4d02128209f99fd2c2b967f985585
Favour Nwachukwu <nwachukwufavour017@gmail.com>
2023-08-20 22:49:43 +0100

    Added support to clean image from local after push to  aws ecr

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -10,15 +10,12 @@
 //    sh """
 //        docker image push ${dockerHubUser}/${imageName}:${imageTag}
 //        docker image push ${dockerHubUser}/${imageName}:latest
-aws ecr get-login-password --region ${aws_region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com
 //    """
 //}
 
 def call(String ecrRepoName, String aws_account_id, String aws_region){
-    def awsCliCommand = "aws ecr get-login-password --region ${aws_region}"
-    def loginCommand = sh(script: awsCliCommand, returnStdout: true).trim()
-    sh script: "${loginCommand} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com"
     sh """
+        aws ecr get-login-password --profile default --region ${aws_region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com
         docker push ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecrRepoName}:latest
     """
 }
