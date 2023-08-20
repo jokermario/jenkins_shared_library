a988f95fbbc40b3b2501cc55458003a846d43471
Favour Nwachukwu <nwachukwufavour017@gmail.com>
2023-08-20 14:47:47 +0100

    Added support to build ecr image

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,14 @@
-def call(String imageName, String imageTag, String dockerHubUser) {
+//def call(String imageName, String imageTag, String dockerHubUser) {
+//    sh """
+//        docker image build -t ${dockerHubUser}/${imageName} .
+//        docker image tag ${dockerHubUser}/${imageName} ${dockerHubUser}/${imageName}:${imageTag}
+//        docker image tag ${dockerHubUser}/${imageName} ${dockerHubUser}/${imageName}:latest
+//    """
+//}
+
+def call(String ecrRepoName, String aws_account_id, String aws_region){
     sh """
-        docker image build -t ${dockerHubUser}/${imageName} .
-        docker image tag ${dockerHubUser}/${imageName} ${dockerHubUser}/${imageName}:${imageTag}
-        docker image tag ${dockerHubUser}/${imageName} ${dockerHubUser}/${imageName}:latest
+        docker build -t ${ecrRepoName} .
+        docker tag ${ecrRepoName}:latest ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecrRepoName}:latest
     """
 }
