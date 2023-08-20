f9fcb1a19b0bd58a3697c94adfaf88f07523e6c9
Favour Nwachukwu <nwachukwufavour017@gmail.com>
2023-08-20 16:17:41 +0100

    Added support to scan ecr image

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,13 @@
-def call(String imageName, String dockerHubUser) {
+//def call(String imageName, String dockerHubUser) {
+//    sh """
+//        trivy image ${dockerHubUser}/${imageName}:latest > scan.txt
+//        cat scan.txt
+//    """
+//}
+
+def call(String ecrRepoName, String aws_account_id, String aws_region){
     sh """
-        trivy image ${dockerHubUser}/${imageName}:latest > scan.txt
-        cat scan.txt
+       trivy image ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecrRepoName}:latest > scan.txt
+       cat scan.txt
     """
 }
