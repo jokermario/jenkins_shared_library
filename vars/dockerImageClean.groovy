0e6d864897c05bd32e11510a96bcaf43be1f0b8b
Favour Nwachukwu <nwachukwufavour017@gmail.com>
2023-08-20 16:31:05 +0100

    Added support to clean image from local after push to  aws ecr

--- vars/dockerImageClean.groovy
+++ vars/dockerImageClean.groovy
@@ -1,6 +1,12 @@
-def call(String imageName, String imageTag, String dockerHubUser) {
+//def call(String imageName, String imageTag, String dockerHubUser) {
+//    sh """
+//        docker rmi ${dockerHubUser}/${imageName}:${imageTag}
+//        docker rmi ${dockerHubUser}/${imageName}:latest
+//    """
+//}
+
+def call(String ecrRepoName, String aws_account_id, String aws_region){
     sh """
-        docker rmi ${dockerHubUser}/${imageName}:${imageTag}
-        docker rmi ${dockerHubUser}/${imageName}:latest
+        docker rmi ${ecrRepoName}:latest ${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${ecrRepoName}:latest
     """
 }
